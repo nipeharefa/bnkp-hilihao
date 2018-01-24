13320bb7456c82d7b1e40ff5190603585bb7b5aa
Nipe Setiawan <nipeharefa@gmail.com>
2018-01-24 23:01:32 +0700

    fix wilcard

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :admin
+  # resources :admin
+  get '/admin/(*id)', to: 'admin#index'
 
   scope '/api', module: 'api', format: :json do
     resources :congregation, :education, :diaken, :baptism
