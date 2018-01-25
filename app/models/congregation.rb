9f63e9fe901f65533da4d66b74720610e30226c5
Nipe Setiawan <nipeharefa@gmail.com>
2018-01-25 09:41:36 +0700

    optional lingkungan

--- app/models/congregation.rb
+++ app/models/congregation.rb
@@ -2,7 +2,7 @@ class Congregation < ApplicationRecord
     paginates_per 50
 
     has_one :baptism
-    belongs_to :lingkungan
+    belongs_to :lingkungan, optional: true
 
     # enum gender: [:man, :woman]
     enum gender: { man: 'man', woman: 'woman' }
